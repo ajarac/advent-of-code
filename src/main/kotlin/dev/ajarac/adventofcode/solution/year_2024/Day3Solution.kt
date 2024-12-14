f9fe4bcbe89ab1916cbd15bcec5bc5dfcbb9109a
Angel Jara <ajarac92@gmail.com>
2024-12-14 17:06:56 +0400

    2024 | Day 3 part 2

--- src/main/kotlin/dev/ajarac/adventofcode/solution/year_2024/Day3Solution.kt
+++ src/main/kotlin/dev/ajarac/adventofcode/solution/year_2024/Day3Solution.kt
@@ -6,9 +6,28 @@ object Day3Solution : Solution(2024, 3, 1) {
     override fun solvePart1(input: List<String>): String {
         val regex: Regex = "mul\\([0-9]+,[0-9]+\\)".toRegex()
         return input
-            .flatMap { line -> regex.findAll(line) }
+            .flatMap { regex.findAll(it) }
             .sumOf { multiply(it.groupValues.first()) }
             .toString()
+    }
+
+    override fun solvePart2(input: List<String>): String {
+        val regex: Regex = "mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\)".toRegex()
+
+        val instructions = input.flatMap { regex.findAll(it) }.map { it.groupValues.first() }
+        var shouldOperate = true
+        var result = 0
+        for(instruction in instructions) {
+            if (instruction.contains("do()")) {
+                shouldOperate = true
+            } else if(instruction.contains("don't()")) {
+                shouldOperate = false
+            } else if(shouldOperate && instruction.contains("mul")) {
+                result += multiply(instruction)
+            }
+        }
+
+        return result.toString()
     }
 
     private fun multiply(input: String): Int {
