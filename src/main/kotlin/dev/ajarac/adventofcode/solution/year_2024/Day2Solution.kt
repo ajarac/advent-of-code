f94d3468d0f4855861a6ece74415e879fec320e9
Angel Jara <ajarac92@gmail.com>
2024-12-14 12:13:19 +0400

    2024 | Day 2 part 2

--- src/main/kotlin/dev/ajarac/adventofcode/solution/year_2024/Day2Solution.kt
+++ src/main/kotlin/dev/ajarac/adventofcode/solution/year_2024/Day2Solution.kt
@@ -4,30 +4,44 @@ import dev.ajarac.adventofcode.solution.Solution
 
 object Day2Solution : Solution(2024, 2, 1) {
     override fun solvePart1(input: List<String>): String {
-        var counter = 0
+        return input.count { list -> isSafeList(list.toListInt()) }.toString()
+    }
 
-        for (list in input) {
-            val numbers = list.split(" ").map { it.toInt() }
+    override fun solvePart2(input: List<String>): String {
+        return input.count { list -> generatePermutations(list.toListInt()).any { isSafeList(it) } }.toString()
+    }
 
-            var isIncreasing = true
-            var isDecreasing = true
-            var previous = numbers.first()
-            for (index in 1..numbers.size - 1) {
-                var compare = numbers[index]
-                if (isIncreasing && previous >= compare || compare - previous > 3) {
-                    isIncreasing = false
+    private fun generatePermutations(input: List<Int>) = sequence {
+        yield(input)
+        for (index in input.indices) {
+            var newList = mutableListOf<Int>()
+            for (i in input.indices) {
+                if (i != index) {
+                    newList.add(input[i])
                 }
-                if (isDecreasing && previous <= compare || previous - compare > 3) {
-                    isDecreasing = false
-                }
-                previous = numbers[index]
             }
+            yield(newList)
+        }
+    }
 
-            if (isIncreasing || isDecreasing) {
-                counter++
+
+    private fun isSafeList(numbers: List<Int>): Boolean {
+        var isIncreasing = true
+        var isDecreasing = true
+        var previous = numbers.first()
+        for (index in 1..numbers.size - 1) {
+            var compare = numbers[index]
+            if (isIncreasing && previous >= compare || compare - previous > 3) {
+                isIncreasing = false
             }
+            if (isDecreasing && previous <= compare || previous - compare > 3) {
+                isDecreasing = false
+            }
+            previous = numbers[index]
         }
 
-        return counter.toString()
+        return isIncreasing || isDecreasing
     }
+
+    private fun String.toListInt() = this.split(" ").map { it.toInt() }
 }
